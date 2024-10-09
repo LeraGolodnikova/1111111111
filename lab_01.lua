7b9603e6bc0b5e57968e6b1dca4f1f4ef9ca93b5
LeraGolodnikova <154059020+LeraGolodnikova@users.noreply.github.com>
2024-10-09 11:20:00 +0300

    Add files via upload

---
+++ lab_01.lua
@@ -0,0 +1,12 @@
+
+lgi = require 'lgi'
+gtk = lgi.Gtk
+gtk.init() 
+bld = gtk.Builder()
+bld:add_from_file('labuzov_lab_01.glade')
+ui=bld.objects
+ui.wnd.title = 'lab_01'
+ui.wnd.on_destroy = gtk.main_quit
+ui.wnd:show_all()
+gtk.main()
+-- Лабузов Глеб 325 гр
